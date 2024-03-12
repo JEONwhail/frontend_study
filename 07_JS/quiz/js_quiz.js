7edc2b197426183e1543a870c94d00e72ce9d27f
JEONwhail <green_jeon247>
2024-03-12 10:53:27 +0900

    커밋

--- 07_JS/quiz/js_quiz.js
+++ 07_JS/quiz/js_quiz.js
@@ -191,4 +191,18 @@ spanEls.forEach(function (spanEls) {
 // 'active'가 포함되어 있으면, '포함됨!'을
 // 'active'가 포함되어 있지않으면, '포함 안됨!'을 콘솔 출력하시오!
 
+// const searchElInput = document.querySelector('input');
+// console.log(searchElInput);
+//문서 전체에서 찾지말고 아래와 같이 최적회
+//각 요소도 각각의 querySelector를 가지고 있음
+const searchElInput = searchEl.querySelector('input');
+console.log(searchElInput);
+
+//검색 영역을 클릭하면 input 요소를 포커스 하도록 실행
+searchEl.addEventListener('click', function () { //이벤트 핸들러
+  searchElInput.focus(); //요소에 포커스 강제 적용
+});
+
+
+
 
