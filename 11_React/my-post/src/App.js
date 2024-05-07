e83cb329a762a1e3d76b71f15466cd9db953f4e6
JEONwhail <green_jeon247@naver.com>
2024-05-07 16:52:28 +0900

    숙제

--- 11_React/my-post/src/App.js
+++ 11_React/my-post/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import PostDetail from './components/PostDetail';
 
 // POST CRUD만들기
 //CRUD -> 앱을 만들때 가장 기본적인 구성
@@ -12,6 +13,12 @@ import { useState } from 'react';
 function App() {
   // 서버에서 가져온 데이터라고 가정
   const [posts, setPosts] = useState(['리액트 잘쓰려면?', '자바스크립트 핵심 문법', 'CSS 스타일링 가이드']);
+
+
+  const [showPostDetail, setShowPostDetail] = useState(false);
+  
+  // 고유한 아이디값이 없기에 인덱스로 연결시킴 
+  const [currentIndet, setCurrentIndet] = useState(null);
 
   return (
     <>
@@ -27,7 +34,8 @@ function App() {
       
       <div className="inner">
         {/* 포스트 목록 */}
-
+        {/* 하드코딩 ver */}
+        {/* 
         <div className='list'>
           <h4> {posts[0]} </h4>
           <p>2024년 05월 07일</p>
@@ -44,17 +52,30 @@ function App() {
           <h4> {posts[2]} </h4>
           <p>2024년 05월 07일</p>
           <p>by JEON</p>
-        </div>
+        </div> */}
 
         {/* Quiz: map() 함수를 이용해서 posts 반복 렌더링 하기 */}
 
         {posts.map((post, index) => (
-          <div key={index} className='list'>
+          <div key={index} className='list'
+          onClick={()=>{
+            setShowPostDetail(true);
+            setCurrentIndet(index); //아이디값이 없으니 index로 대신함.
+          }}>
             <h4> {post} </h4>
             <p>2024년 05월 07일</p>
             <p>by JEON</p>
           </div>
         ))}
+
+        {/* 포스트 상세보기 */}
+        {/* Quiz 조건부 렌더링으로 list 클릭했을 때 showPostDetail이 true로 바뀌고 true로 바뀌면 화면에 나오도록 */}
+        {showPostDetail && <PostDetail posts={posts}
+        currentIndex={currentIndet} />}
+
+        {/* Quiz 제목 가져오기 */}
+
+
 
       </div>
     </>
---
+++ 11_React/my-post/src/components/PostDetail.jsx
@@ -0,0 +1,13 @@
+function PostDetail() {
+  return (
+    <div className="detail">
+      <h4>제목 : </h4>
+      <p>날짜 : 2024년 05월 07일</p>
+      <p>작성자 : JEON</p>
+      <p>... 상세내용 ...</p>
+
+    </div>
+  );
+};
+
+export default PostDetail;
